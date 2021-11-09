3a455d374bd6acd5a26cc1052163b813f2bb4108
Nejc Drobnic <ethoarx@gmail.com>
2021-11-09 18:35:39 +0100

    fix: disable editable commands

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -1,6 +1,6 @@
 import '@sapphire/plugin-logger/register';
 import '@sapphire/plugin-api/register';
-import '@skyra/editable-commands/register';
+// import '@skyra/editable-commands/dist/index.mjs';
 import * as colorette from 'colorette';
 import { inspect } from 'util';
 
